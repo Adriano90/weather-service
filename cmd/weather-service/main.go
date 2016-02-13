1f0b8f99baf4b50f4fa94441726e912a5a259f39
Adriano90 <elamodeltinto@hotmail.com>
2016-02-13 13:45:18 +0100

    updated procfile and port

--- cmd/weather-service/main.go
+++ cmd/weather-service/main.go
@@ -6,9 +6,16 @@ import (
 	"github.com/Adriano90/weather-service/cmd/weather-service/usecases"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
 	restHandler := &http.Client{}
 	handlers := make(map[string]interfaces.RestHandler)
@@ -24,5 +31,5 @@ func main() {
 	mux.Get("/forecast", webserviceHandler.GetForecast)
 	http.Handle("/", mux)
 	log.Println("Bootstrapping weather-service...")
-	log.Fatal(http.ListenAndServe(":8088", nil))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
